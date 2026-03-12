c7dedfd74b5e47a882eda221d8edc69480daebbe
KhangDepZai18 <khangld@gmail.com>
2026-03-12 23:58:17 +0700

    Update config.php

--- bookept-main/config.php
+++ bookept-main/config.php
@@ -3,5 +3,6 @@
 // $conn = mysqli_connect('localhost:3325', 'root', '', 'shop_db') or die('connection failed');
 //Production
 $conn = mysqli_connect("127.0.0.1", "root","", "shop_db") or die('connection failed');
-
+//fix tiếng Việt có dấu
+mysqli_set_charset($conn, "utf8mb4");
 ?>
